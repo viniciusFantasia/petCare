07490dfbb611388c1875defc185277a19ffc270f
Fantasia <vinicius.fantasia10@gmail.com>
2020-07-07 11:52:03 -0300

    css especie

--- vacinas/excluirvacinas.php
+++ vacinas/excluirvacinas.php
@@ -5,34 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet Care</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body class="textocentralizado">
-<div id="fundo-externo">
+    <div id="fundo-externo">
         <div id="fundo">
             <img src="../imagens/usuario.jpg" alt="" />
         </div>
     </div>
     <div id="site">
-    <h1>Pet Care</h1>
-    <?php
-    if (isset($_GET['idvacina'])) {
-        $idusuario = $_GET['idvacina'];
+        <h1>Pet Care</h1>
+        <?php
+        if (isset($_GET['idvacina'])) {
+            $idusuario = $_GET['idvacina'];
 
-         //montar a instrução SQL
-         $sql="delete from vacinas where idvacina=$idvacina";
-         //echo $sql;
-         require_once "../conexao.php";
-         $conn->exec($sql);
-         echo "<p>Excluido com sucesso</p>";
-    } else {
-        echo "<p> Erro aos receber os dados!!! <p>";
-    }
-    ?>
-    <br>
-    <a href="cadvacina.php">Voltar</a><br>
-    <a href="../index.php">Home</a><br>
+            //montar a instrução SQL
+            $sql = "delete from vacinas where idvacina=$idvacina";
+            //echo $sql;
+            require_once "../conexao.php";
+            $conn->exec($sql);
+            echo "<p>Excluido com sucesso</p>";
+        } else {
+            echo "<p> Erro aos receber os dados!!! <p>";
+        }
+        ?>
+        <br>
+        <a href="cadvacina.php">Voltar</a><br>
+        <a href="../index.php">Home</a><br>
     </div>
 </body>
 
